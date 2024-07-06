1265600e3b1199be85fd5dd56840991d1107e914
CodingSpencer <sashcraft121@gmail.com>
2024-07-06 16:54:01 -0600

    first commit

---
+++ scripts/projects.mjs
@@ -0,0 +1,82 @@
+const recipes = [
+  {
+    name: "Spotify Recommendation",
+    github: "https://github.com/CodingSpencer/web_spotify.git",
+    lastUpdate: "07/03/2024",
+    tags: ["API", "Spotify", "JavaScript"],
+    description:
+      "Used Spotify's API to create my own personalized recommendation method.",
+    image: "images/spotify.png",
+    type: "Personal",
+  },
+  {
+    name: "Spotify Recommendation",
+    github: "https://github.com/CodingSpencer/web_spotify.git",
+    lastUpdate: "07/03/2024",
+    tags: ["API", "Spotify", "JavaScript"],
+    description:
+      "Used Spotify's API to create my own personalized recommendation method.",
+    image: "images/spotify.png",
+    type: "personal",
+  },
+  {
+    name: "Spotify Recommendation",
+    github: "https://github.com/CodingSpencer/web_spotify.git",
+    lastUpdate: "07/03/2024",
+    tags: ["API", "Spotify", "JavaScript"],
+    description:
+      "Used Spotify's API to create my own personalized recommendation method.",
+    image: "images/spotify.png",
+    type: "personal",
+  },
+  {
+    name: "Spotify Recommendation",
+    github: "https://github.com/CodingSpencer/web_spotify.git",
+    lastUpdate: "07/03/2024",
+    tags: ["API", "Spotify", "JavaScript"],
+    description:
+      "Used Spotify's API to create my own personalized recommendation method.",
+    image: "images/spotify.png",
+    type: "personal",
+  },
+  {
+    name: "Spotify Recommendation",
+    github: "https://github.com/CodingSpencer/web_spotify.git",
+    lastUpdate: "07/03/2024",
+    tags: ["API", "Spotify", "JavaScript"],
+    description:
+      "Used Spotify's API to create my own personalized recommendation method.",
+    image: "images/spotify.png",
+    type: "personal",
+  },
+  {
+    name: "Spotify Recommendation",
+    github: "https://github.com/CodingSpencer/web_spotify.git",
+    lastUpdate: "07/03/2024",
+    tags: ["API", "Spotify", "JavaScript"],
+    description:
+      "Used Spotify's API to create my own personalized recommendation method.",
+    image: "images/spotify.png",
+    type: "personal",
+  },
+  {
+    name: "Spotify Recommendation",
+    github: "https://github.com/CodingSpencer/web_spotify.git",
+    lastUpdate: "07/03/2024",
+    tags: ["API", "Spotify", "JavaScript"],
+    description:
+      "Used Spotify's API to create my own personalized recommendation method.",
+    image: "images/spotify.png",
+    type: "personal",
+  },
+  {
+    name: "Spotify Recommendation",
+    github: "https://github.com/CodingSpencer/web_spotify.git",
+    lastUpdate: "07/03/2024",
+    tags: ["API", "Spotify", "JavaScript"],
+    description:
+      "Used Spotify's API to create my own personalized recommendation method.",
+    image: "images/spotify.png",
+    type: "personal",
+  },
+];
