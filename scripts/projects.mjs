656b0652ad6d4d1728e45f23bdc69760b09a5303
CodingSpencer <sashcraft121@gmail.com>
2024-10-11 09:16:43 -0600

    resume projects and images added

--- scripts/projects.mjs
+++ scripts/projects.mjs
@@ -5,7 +5,7 @@ export const projects = [
     lastUpdate: "07/03/2024",
     tags: ["API", "Spotify", "JavaScript"],
     description:
-      "Used Spotify's API to create my own personalized recommendation method.",
+      "Used Spotify's API to create my own personalized recommendation method using Javascript. Created a new playlist based on preferences of users for different genres or styles.",
     image: "images/spotify.avif",
     type: "Personal",
   },
@@ -15,18 +15,28 @@ export const projects = [
     lastUpdate: "07/19/2024",
     tags: ["Unity", "Team Project", "C#"],
     description:
-      "Created a game in a team setting while working in a team using an Agile structure to complete the project.",
+      "Created a game in a team setting while working in a team using an Agile structure to complete the project. 13 week project in which we learned to manage team management and Unity. I was assigned to testing while working on this project.",
     image: "images/unity.avif",
+    type: "team",
+  },
+  {
+    name: "Ventoy",
+    github: "N/A",
+    lastUpdate: "10/09/2024",
+    tags: ["Linux", "Bootable Flash Drive", "Terminal"],
+    description:
+      "Ventoy is a powerful program for creating bootable flash drives that allows users to boot multiple operating system images. Primarily designed for Linux ISO images, Ventoy enables you to try out various operating systems or install them directly. The flash drive can be plugged into any computer and is easily configurable through the BIOS settings.",
+    image: "images/Ventoy.jpg",
     type: "personal",
   },
   {
-    name: "Chess in Unity",
-    github: "https://github.com/JaredWightman/Chessboard.git",
-    lastUpdate: "07/19/2024",
-    tags: ["Unity", "Team Project", "C#"],
+    name: "Plex Server",
+    github: "N/A",
+    lastUpdate: "05/04/2024",
+    tags: ["Linux", "Server", "Terminal", "Permissions"],
     description:
-      "Created a game in a team setting while working in a team using an Agile structure to complete the project.",
-    image: "images/unity.avif",
+      "Plex Servers allow for users to have a computer broadcast owned movies and shows over a network. I used a spare PC and have it broadcasting my media for me to view anywhere with a internet connection.",
+    image: "images/plex.jpg",
     type: "personal",
   },
 ];
